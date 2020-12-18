189d9971d1c78d6c74721c728b4e158338d9819a
oMateus006 <mateus.ribeiro@aluno.ufop.edu.br>
2020-12-18 17:23:48 -0300

    another update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component }from 'react';
 
 import Routes from './routes';
 
---
+++ src/components/menu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Menu extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Menu</h1>
+            </div>
+        );
+    }
+}
